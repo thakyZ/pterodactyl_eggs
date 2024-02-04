3e67a8fe292f419b6787e078300809bb72863757
thakyZ <950594+thakyZ@users.noreply.github.com>
2024-02-03 16:23:31 -0800

    add: palworld manager and pst scripts

---
+++ managers/palworld/download_latest.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+""" Download the latest release assets from GitHub """
+
+import json
+import os
+from pathlib import Path
+from re import Pattern
+import re
+from argparse import Action, ArgumentParser, ArgumentError, Namespace
+import urllib.request as urllib
+import requests
+
+repo_argument: Action
+output_argument: Action
+file_argument: Action
+
+def validate_url(value: str) -> list[str]:
+    """ validates the github url. """
+    site, path = value.split(":")
+    user, repo = path.split("/")
+    if site != "gh":
+        raise ArgumentError(repo_argument, "repo is not a valid github argument")
+    return [site, user, repo]
+
+def validate_dir(value: str) -> Path:
+    """ validates the directory to output assets to. """
+    temp_path: Path = Path(value)
+    if not temp_path.exists():
+        os.makedirs(temp_path, exist_ok=True)
+    elif not temp_path.is_dir():
+        raise ArgumentError(output_argument, "Output directory is not a directory.")
+    return temp_path
+
+def validate_regex(value: str) -> Pattern[str]:
+    """ validates the directory to output assets to. """
+    if value.startswith("r'") and value.endswith("'"):
+        return re.compile(value.removeprefix("r'").removesuffix("'"))
+    return re.compile(re.escape(value))
+
+def fetch_api_key() -> str:
+    """ fetches api key from file named '.env' in the same directory as the script """
+    script_dir = Path(os.path.dirname(os.path.realpath(__file__)))
+    env_file = Path(os.path.join(script_dir, ".env"))
+
+    output: str = ""
+
+    with env_file.open("r", encoding="utf8") as fr:
+        output = fr.readline()
+        fr.close()
+    return output
+
+if __name__ == "__main__":
+
+    argparse = ArgumentParser(prog="download_latest", description="Download the latest release assets from GitHub")
+    repo_argument = argparse.add_argument("--repo", "-r", type=validate_url, required=True)
+    output_argument = argparse.add_argument("--output_dir", "-o", dest="output", type=validate_dir, required=True)
+    file_argument = argparse.add_argument("--file", "-f", nargs="+", dest="assets", help="<Required> Set flag", type=validate_regex, required=True)
+    args: Namespace = argparse.parse_args()
+
+    api_key: str = fetch_api_key()
+
+    url: str = f"http://api.github.com/repos/{args.repo[1]}/{args.repo[2]}/releases"
+    headers: dict[str, str] = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {api_key}",
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+
+    response = requests.get(url, headers=headers, timeout=10000)
+    json_data = json.loads(response.content.decode(response.apparent_encoding))
+    to_dl_assets: dict[str, str] = {}
+
+    reg: Pattern[str]
+    for reg in args.assets:
+        for asset in json_data[0]["assets"]:
+            if reg.match(asset["name"]) is not None:
+                to_dl_assets[asset["name"]] = asset["browser_download_url"]
+
+    for item, url in to_dl_assets.items():
+        download_file = urllib.urlretrieve(url, Path(os.path.join(args.output, item)))
---
+++ managers/palworld/pst/setup_config.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python3
+
+# pyright: reportUnknownMemberType=false
+# cSpell:ignore representer
+""" P """
+
+import re
+from re import Pattern
+import os
+from pathlib import Path
+from argparse import ArgumentTypeError, Namespace, ArgumentParser
+from typing import Any
+import yaml
+from yaml.dumper import Dumper
+import yaml.representer
+from yaml.representer import ScalarNode
+from yaml import YAMLError
+
+startup_run: bool = str(os.getenv("installing")) == "true"
+
+def validate_bool_str(value: str) -> bool:
+    """ P """
+    if value.lower() == "true":
+        return True
+    if value.lower() != "false":
+        raise ArgumentTypeError("Argument is not a \"false\" or \"true\" statement.")
+    return False
+
+def validate_int(value: str) -> int:
+    """ P """
+    return int(value, base=10)
+
+def validate_ip_str(value: str) -> str:
+    """ P """
+    regex: Pattern[str] = re.compile(r"^(?:(?:(?:25[0-5]|(?:2[0-4]|1\d|[1-9]|)\d)(?:\.(?!$)|$)){4}|(?:(?:[a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*(?:[A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])|(?:::01))$")
+    if regex.match(value) is None:
+        raise ArgumentTypeError("Argument of type ip address does not match a required string")
+    return value
+
+def validate_port_str(value: str) -> str:
+    """ P """
+    temp_int: int = int(value, base=10)
+    if temp_int < 255 or temp_int > 65535:
+        raise ArgumentTypeError("Argument of type port needs to be greater than 255 and less than 65535")
+    return value
+
+def validate_file(value: str) -> Path:
+    """ P """
+    if not startup_run:
+        temp_path: Path = Path(value)
+        if temp_path.exists() and temp_path.is_file():
+            return temp_path
+        else:
+            if not temp_path.exists():
+                raise ArgumentTypeError(f"File {temp_path} does not exist on the file system.")
+            if not temp_path.is_file():
+                raise ArgumentTypeError(f"File {temp_path} does not is not a file on the file system.")
+            raise ArgumentTypeError(f"File {temp_path} has some other issue on the file system.")
+    return Path(value)
+
+def validate_file_exists(value: str) -> Path:
+    """ P """
+    temp_path: Path = Path(value)
+    if temp_path.exists() and temp_path.is_file():
+        return temp_path
+    if not temp_path.exists():
+        raise ArgumentTypeError(f"File {temp_path} does not exist on the file system.")
+    if not temp_path.is_file():
+        raise ArgumentTypeError(f"File {temp_path} does not is not a file on the file system.")
+    raise ArgumentTypeError(f"File {temp_path} has some other issue on the file system.")
+
+def validate_directory_exists(value: str) -> Path:
+    """ P """
+    temp_path: Path = Path(value)
+    if temp_path.exists() and temp_path.is_dir():
+        return temp_path
+    if not temp_path.exists():
+        raise ArgumentTypeError(f"File {temp_path} does not exist on the file system.")
+    if not temp_path.is_dir():
+        raise ArgumentTypeError(f"File {temp_path} does not is not a directory on the file system.")
+    raise ArgumentTypeError(f"File {temp_path} has some other issue on the file system.")
+
+if __name__ == "__main__":
+    argparse = ArgumentParser(prog="setup_config", description="Parses arguments from command line and writes to a yaml file.")
+    argparse.add_argument("--io", type=validate_file_exists, required=True)
+    web_argument_group = argparse.add_argument_group("web", description="")
+    web_argument_group.add_argument("--web_password", "-p", type=str, required=True)
+    web_argument_group.add_argument("--web_tls", "-t", type=validate_bool_str, required=True)
+    web_argument_group.add_argument("--web_cert_path", "-c", type=validate_file, required=False)
+    web_argument_group.add_argument("--web_key_path", "-k", type=validate_file, required=False)
+    rcon_argument_group = argparse.add_argument_group("rcon", description="")
+    rcon_argument_group.add_argument("--rcon_address", "-a", type=validate_ip_str, required=True)
+    rcon_argument_group.add_argument("--rcon_port", "-P", type=validate_port_str, required=True)
+    rcon_argument_group.add_argument("--rcon_password", "-A", type=str, required=True)
+    rcon_argument_group.add_argument("--rcon_timeout", "-T", type=validate_int, required=True)
+    rcon_argument_group.add_argument("--rcon_sync_interval", "-s", type=validate_int, required=True)
+    save_argument_group = argparse.add_argument_group("save", description="")
+    save_argument_group.add_argument("--save_path", "-d", type=validate_directory_exists, required=True)
+    save_argument_group.add_argument("--save_decode_path", "-D", type=validate_file_exists, required=True)
+    save_argument_group.add_argument("--save_sync_interval", "-S", type=validate_int, required=True)
+    args: Namespace = argparse.parse_args()
+
+    config: Path = Path(args.io)
+
+    if not config.exists():
+        raise FileNotFoundError(f"The file `config.yaml` was not found at path `{config}`")
+
+    yaml_data: dict[str, Any] | None
+
+    with config.open("r", encoding="utf8") as stream_read:
+        yaml_data = yaml.full_load(stream_read)
+
+    if yaml_data is None:
+        raise YAMLError("Failed to parse config file.")
+
+    def convert_to_blank_if_none(value: Any | None) -> str:
+        """ a """
+        if value is None:
+            return ""
+        return str(value)
+
+    def literal_presenter(dumper: Dumper, data: Any) -> ScalarNode:
+        """ a """
+        if isinstance(data, str) and "\n" in data:
+            return dumper.represent_scalar("tag:yaml.org,2002:str", data, style="|")
+        return dumper.represent_scalar("tag:yaml.org,2002:str", data, style="\"")
+
+    yaml_data["web"]["password"] = args.web_password
+    yaml_data["web"]["tls"] = args.web_tls
+    yaml_data["web"]["cert_path"] = convert_to_blank_if_none(args.web_cert_path)
+    yaml_data["web"]["key_path"] = convert_to_blank_if_none(args.web_key_path)
+    yaml_data["rcon"]["address"] = f"{args.rcon_address}:{args.rcon_port}"
+    yaml_data["rcon"]["password"] = args.rcon_password
+    yaml_data["rcon"]["timeout"] = args.rcon_timeout
+    yaml_data["rcon"]["sync_interval"] = args.rcon_sync_interval
+    yaml_data["save"]["path"] = convert_to_blank_if_none(args.save_path)
+    yaml_data["save"]["decode_path"] = convert_to_blank_if_none(args.save_decode_path)
+    yaml_data["save"]["sync_interval"] = args.save_sync_interval
+
+    yaml.add_representer(str, literal_presenter)
+
+    with config.open("w", encoding="utf8") as stream_write:
+        yaml.dump(yaml_data, stream_write, default_flow_style=False, allow_unicode=True, encoding="utf8")
+        stream_write.close()
